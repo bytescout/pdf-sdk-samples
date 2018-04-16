bf208db2da604923ef7a77905ddf37208eb3bedf
estar73 <estarkov@gmail.com>
2018-04-16 04:26:17 -0700

    = regular update

---
+++ _Web API/HTML To PDF/VB.NET/Convert Web Page To PDF From URL/Module1.vb
@@ -0,0 +1,78 @@
+'*****************************************************************************************'
+'                                                                                         '
+' Download offline evaluation version (DLL): https://bytescout.com/download/web-installer '
+'                                                                                         '
+' Signup Web API free trial: https://secure.bytescout.com/users/sign_up                   '
+'                                                                                         '
+' Copyright © 2017-2018 ByteScout Inc. All rights reserved.                               '
+' http://www.bytescout.com                                                                '
+'                                                                                         '
+'*****************************************************************************************'
+
+
+Imports System.IO
+Imports System.Net
+Imports Newtonsoft.Json.Linq
+
+Module Module1
+
+	' (!) If you are getting '(403) Forbidden' error please ensure you have set the correct API_KEY
+
+	' The authentication key (API Key).
+	' Get your own by registering at https://secure.bytescout.com/users/sign_up
+	Const API_KEY As String = "***********************************"
+
+	' URL of web page to convert to PDF document.
+	const SourceUrl as String = "http://www.usa.gov"
+	' Destination PDF file name
+	Const DestinationFile As String = ".\result.pdf"
+
+	Sub Main()
+
+		' Create standard .NET web client instance
+		Dim webClient As WebClient = New WebClient()
+
+		' Set API Key
+		webClient.Headers.Add("x-api-key", API_KEY)
+
+		' Prepare URL for `Web Page to PDF` API call
+		Dim query As String = Uri.EscapeUriString(String.Format(
+			"https://bytescout.io/v1/pdf/convert/from/url?name={0}&url={1}",
+			Path.GetFileName(DestinationFile),
+			SourceUrl))
+
+		Try
+			' Execute request
+			Dim response As String = webClient.DownloadString(query)
+
+			' Parse JSON response
+			Dim json As JObject = JObject.Parse(response)
+
+			If json("error").ToObject(Of Boolean) = False Then
+
+				' Get URL of generated PDF file
+				Dim resultFileUrl As String = json("url").ToString()
+
+				' Download PDF file
+				webClient.DownloadFile(resultFileUrl, DestinationFile)
+
+				Console.WriteLine("Generated PDF file saved as ""{0}"" file.", DestinationFile)
+
+			Else
+				Console.WriteLine(json("message").ToString())
+			End If
+
+		Catch ex As WebException
+			Console.WriteLine(ex.ToString())
+		End Try
+
+		webClient.Dispose()
+
+
+		Console.WriteLine()
+		Console.WriteLine("Press any key...")
+		Console.ReadKey()
+
+	End Sub
+
+End Module
