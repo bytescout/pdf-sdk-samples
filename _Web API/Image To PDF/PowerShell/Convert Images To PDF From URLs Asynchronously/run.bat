9d7104c0761916a4b959acee2919130e564b0e66
estar73 <estarkov@gmail.com>
2019-01-21 12:59:28 +0000

    = regular update

--- _Web API/Image To PDF/PowerShell/Convert Images To PDF From URLs Asynchronously/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-powershell -NoProfile -ExecutionPolicy Bypass -Command "& .\ConvertImagesToPdfFromUrlsAsynchronously.ps1"
-echo Script finished with errorlevel=%errorlevel%
-
-pause
--- _Web API/Image To PDF/PowerShell/Convert Images To PDF From URLs/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-powershell -NoProfile -ExecutionPolicy Bypass -Command "& .\ConvertImagesToPdfFromUrls.ps1"
-echo Script finished with errorlevel=%errorlevel%
-
-pause
